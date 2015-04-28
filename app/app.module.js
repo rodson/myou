fb66a0bf9d34b208e15312a6ab52131e34f7b6fa
rodson <rodsonluo@gmail.com>
2015-04-28 16:09:02 +0800

    add login page ui

--- app/app.module.js
+++ app/app.module.js
@@ -4,13 +4,38 @@
   angular
     .module('myou', [
       'ui.router',
+      'ngMaterial',
       'app.version',
       'myou.login'
     ])
     .config(mainConfig);
 
-  function mainConfig($urlRouterProvider) {
+  function mainConfig($urlRouterProvider, $mdThemingProvider) {
     $urlRouterProvider.otherwise('/login');
+
+    // Config default theme
+    $mdThemingProvider.definePalette('holoLight', {
+      '50': '40BAFA',
+      '100': '40BAFA',
+      '200': '40BAFA',
+      '300': '40BAFA',
+      '400': '40BAFA',
+      '500': '40BAFA',
+      '600': '40BAFA',
+      '700': '40BAFA',
+      '800': '40BAFA',
+      '900': '40BAFA',
+      'A100': '40BAFA',
+      'A200': '40BAFA',
+      'A400': '40BAFA',
+      'A700': '40BAFA',
+      'contrastDefaultColor': 'light',
+      'contrastDarkColors': ['50', '100',
+       '200', '300', '400', 'A100'],
+      'contrastLightColors': undefined
+    });
+    $mdThemingProvider.theme('default')
+      .primaryPalette('holoLight');
   }
 
 })();
