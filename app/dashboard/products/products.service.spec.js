1efbe96c290026afeff9c2c07fd79700d556e4ce
rodson <rodsonluo@gmail.com>
2015-05-11 16:55:12 +0800

    add the unit test for enterProduct function in ProductsService

--- app/dashboard/products/products.service.spec.js
+++ app/dashboard/products/products.service.spec.js
@@ -4,14 +4,19 @@ describe('ProductsService: ', function() {
   var $httpBackend;
   var ProductsService;
   var Constant;
+  var StateManager;
 
   beforeEach(function() {
     module('myou.dashboard.products');
 
-    inject(function(_$httpBackend_, _ProductsService_, _Constant_) {
+    inject(function(_$httpBackend_, _ProductsService_,
+      _StateManager_, _Constant_) {
+
       $httpBackend = _$httpBackend_;
       ProductsService = _ProductsService_;
       Constant = _Constant_;
+      StateManager = _StateManager_;
+
     });
   });
 
@@ -20,7 +25,7 @@ describe('ProductsService: ', function() {
     $httpBackend.verifyNoOutstandingRequest();
   });
 
-  describe('fn: getProduct', function() {
+  describe('fn: getProduct: ', function() {
 
     it('should return all products when ownerId is not passed', function() {
       var allProducts = {
@@ -45,6 +50,24 @@ describe('ProductsService: ', function() {
       ProductsService.getProducts('abc');
       $httpBackend.flush();
       expect(ProductsService.products).toEqual(groupProducts);
+
+    });
+  });
+
+  describe('fn: enterProduct: ', function() {
+    beforeEach(function() {
+      spyOn(StateManager, 'enterProduct');
+    });
+
+    it('should call enterProduct on StateManager', function() {
+      var product = {
+        platform: 'android_app',
+        _id: 'qwerty'
+      };
+
+      ProductsService.enterProduct(product);
+      expect(StateManager.enterProduct)
+        .toHaveBeenCalledWith('android_app', 'qwerty');
 
     });
   });
