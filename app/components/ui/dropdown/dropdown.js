b04e243153f2fe843bd923da54c2592fd816f99e
rodson <rodsonluo@gmail.com>
2015-05-18 15:00:12 +0800

    add basic ui for update config page

---
+++ app/components/ui/dropdown/dropdown.js
@@ -0,0 +1,26 @@
+(function() {
+  'use strict';
+
+  angular
+    .module('myou.ui.dropdown', [])
+    .directive('rsDropdown', RsDropdownDirective);
+
+  function RsDropdownDirective() {
+    return {
+      restrict: 'EA',
+      transclude: true,
+      template: [
+        '<rs-dropdown">',
+          '<span class="drop-label">',
+            '<img src="assets/images/material-other/ic_more_vert_18px.svg" alt="more" />',
+          '</span>',
+          '<div class="drop-content">',
+            '<ul ng-transclude></ul>',
+          '</div>',
+        '</rs-dropdown>'
+      ].join('')
+    };
+
+  }
+
+})();
