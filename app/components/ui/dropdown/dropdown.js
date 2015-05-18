e4c1691870ce6f3ec984b036b63504cc538b577b
rodson <rodsonluo@gmail.com>
2015-05-18 18:46:53 +0800

    fix ui

--- app/components/ui/dropdown/dropdown.js
+++ app/components/ui/dropdown/dropdown.js
@@ -11,7 +11,7 @@
       transclude: true,
       template: [
         '<rs-dropdown">',
-          '<span class="drop-label">',
+          '<span class="drop-label extra-width">',
             '<img src="assets/images/material-other/ic_more_vert_18px.svg" alt="more" />',
           '</span>',
           '<div class="drop-content">',
