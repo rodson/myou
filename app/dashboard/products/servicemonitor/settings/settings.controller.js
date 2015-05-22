19881c55338f5879525cd2afc1d9adb8dd3fa9e8
FeichaoWan <len.may@foxmail.com>
2015-05-22 11:01:14 +0800

    setting page

--- app/dashboard/products/servicemonitor/settings/settings.controller.js
+++ app/dashboard/products/servicemonitor/settings/settings.controller.js
@@ -11,8 +11,43 @@
       });
   }
 
-  function SettingsCtrl() {
+  function SettingsCtrl(localStorageService, $mdDialog, SettingsService) {
+    var vm = this;
+    var appKey = localStorageService.get('app').appKey;
+    var appId = localStorageService.get('appId');
 
+    vm.getMonitorConfig = function() {
+      SettingsService.getMonitorConfig(appKey, function() {
+        vm.message = SettingsService.data.msg_alert;
+        vm.email = SettingsService.data.email_alert;
+      });
+    };
+
+    vm.getMonitorConfig();
+
+    vm.setMonitor = function() {
+      var config = {
+        msg_alert: vm.message,
+        email_alert: vm.email,
+        app_id: appId
+      };
+      SettingsService.modifyMonitorConfig(appKey, config, function(error) {
+        if(error) {
+          vm.showAlert(error);
+          vm.getContacts();
+        }
+      });
+    };
+
+    vm.showAlert = function(error) {
+      $mdDialog.show(
+        $mdDialog.alert()
+        .title('Error')
+        .content(error.message)
+        .ariaLabel('Alert Dialog')
+        .ok('关闭')
+      );
+    };
   }
 
   angular
---
+++ app/dashboard/products/servicemonitor/settings/settings.service.js
@@ -0,0 +1,32 @@
+(function() {
+  'use strict';
+
+  angular.module('myou.dashboard.servicemonitor')
+    .factory('SettingsService', SettingsService);
+
+  function SettingsService($http, Constant) {
+    var MONITOR_CONFIG = 'monitor_config';
+
+    var settingsService = {};
+
+    settingsService.getMonitorConfig = function(appKey, cb) {
+      return $http.get(Constant.URL.PRODUCTS_SERVICE_MONITOR + '/' + appKey + '/' + MONITOR_CONFIG)
+        .success(function(data) {
+          settingsService.data = data;
+        });
+    };
+
+    settingsService.modifyMonitorConfig = function(appKey, config, cb) {
+      return $http.put(Constant.URL.PRODUCTS_SERVICE_MONITOR + '/' + appKey + '/' + MONITOR_CONFIG, config)
+        .success(function(data) {
+          cb(null);
+        })
+        .error(function(error) {
+          cb(error);
+        });
+    };
+
+    return settingsService;
+  }
+
+})();
