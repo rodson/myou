7646559fbc54c19d58319e15c5940035a332da84
rodson <rodsonluo@gmail.com>
2015-05-29 20:03:23 +0800

    添加系统更新配子模块

--- app/dashboard/products/systemupdate/updatesetting/updatesetting.controller.js
+++ app/dashboard/products/systemupdate/updatesetting/updatesetting.controller.js
@@ -18,6 +18,30 @@
     RomUpdateSettingService.init();
 
     vm.updateInfos = RomUpdateSettingService.updateInfos;
+
+    vm.showVersion = function(version) {
+      return RomUpdateSettingService.showVersion(version);
+    };
+
+    vm.toggleSilentDownload = function(ev, targetItem) {
+      RomUpdateSettingService.toggleSilentDownload(ev, targetItem);
+    };
+
+    vm.toggleTestUpdatable = function(ev, targetItem, updateInfo) {
+      RomUpdateSettingService.toggleTestUpdatable(ev, targetItem, updateInfo);
+    };
+
+    vm.toggleUpdatable = function(ev, targetItem, updateInfo) {
+      RomUpdateSettingService.toggleUpdatable(ev, targetItem, updateInfo);
+    };
+
+    vm.showUpdateDescDialog = function(ev, targetItem) {
+      RomUpdateSettingService.showUpdateDescDialog(ev, targetItem);
+    };
+
+    vm.showDeleteUpdateDialog = function(ev, targetItem) {
+      RomUpdateSettingService.showDeleteUpdateDialog(ev, targetItem);
+    };
   }
 
   RomUpdateSettingCtrl.resolve = {
@@ -26,9 +50,47 @@
     }
   };
 
+  function RomUpdateDescDialogCtrl($mdDialog, data, RomUpdateSettingService) {
+    var vm = this;
+    vm.updateDesc = data.updateDesc;
+
+    vm.ok = function() {
+      if (vm.updateDesc === data.updateDesc) {
+        $mdDialog.cancel();
+      } else {
+        RomUpdateSettingService.modifyUpdateInfo(data.updateId, {updateDesc: vm.updateDesc})
+          .then(function() {
+            $mdDialog.hide(vm.updateDesc);
+          });
+      }
+    };
+
+    vm.cancel = function() {
+      $mdDialog.cancel();
+    };
+  }
+
+  function RomDeleteUpdateDialogCtrl($mdDialog, RomUpdateSettingService, data) {
+    var vm = this;
+
+    vm.targetItem = data.targetItem;
+
+    vm.ok = function() {
+      RomUpdateSettingService.deleteUpdateInfo(data.updateId).then(function() {
+        $mdDialog.hide();
+      });
+    };
+
+    vm.cancel = function() {
+      $mdDialog.cancel();
+    };
+  }
+
   angular
     .module('myou.dashboard.systemupdate')
     .controller('RomUpdateSettingCtrl', RomUpdateSettingCtrl)
+    .controller('RomUpdateDescDialogCtrl', RomUpdateDescDialogCtrl)
+    .controller('RomDeleteUpdateDialogCtrl', RomDeleteUpdateDialogCtrl)
     .config(romUpdateSettingConfig);
 
 })();
--- app/dashboard/products/systemupdate/updatesetting/updatesetting.service.js
+++ app/dashboard/products/systemupdate/updatesetting/updatesetting.service.js
@@ -1,7 +1,7 @@
 (function() {
   'use strict';
 
-  function RomUpdateSettingService($http, $mdDialog, UrlManager,
+  function RomUpdateSettingService($http, $mdDialog, UrlManager, $state, $stateParams,
     StorageManager) {
 
     var RomUpdateSettingService = {};
@@ -21,6 +21,121 @@
           RomUpdateSettingService.updateInfos = updateInfos;
         });
     };
+
+    RomUpdateSettingService.showVersion = function(version) {
+      if (version) {
+        return version;
+      } else {
+        return '未设置';
+      }
+    };
+
+    RomUpdateSettingService.toggleSilentDownload = function(ev, targetItem) {
+      modifyUpdateInfo(targetItem._id, {isSilentDownload: targetItem.isSilentDownload})
+        .error(function(err) {
+          targetItem.isSilentDownload = !targetItem.isSilentDownload;
+          showDialog(ev, '修改失败', err.message);
+        });
+    };
+
+    RomUpdateSettingService.toggleTestUpdatable = function(ev, targetItem, updateInfo) {
+      modifyUpdateInfo(targetItem._id, {testUpdatable: targetItem.testUpdatable})
+        .success(function() {
+          if (targetItem.testUpdatable) {
+            updateInfo.testTargetVersion = targetItem.targetVersion;
+          } else {
+            updateInfo.testTargetVersion = '';
+          }
+        })
+        .error(function(err) {
+          targetItem.testUpdatable = !targetItem.testUpdatable;
+          showDialog(ev, '修改失败', err.message);
+        });
+    };
+
+    RomUpdateSettingService.toggleUpdatable = function(ev, targetItem, updateInfo) {
+      modifyUpdateInfo(targetItem._id, {updatable: targetItem.updatable})
+        .success(function() {
+          if (targetItem.updatable) {
+            updateInfo.targetVersion = targetItem.targetVersion;
+          } else {
+            updateInfo.targetVersion = '';
+          }
+        })
+        .error(function(err) {
+          targetItem.updatable = !targetItem.updatable;
+          showDialog(ev, '修改失败', err.message);
+        });
+    };
+
+    RomUpdateSettingService.showUpdateDescDialog = function(ev, targetItem) {
+      $mdDialog.show({
+        controller: 'RomUpdateDescDialogCtrl',
+        controllerAs: 'vm',
+        templateUrl: 'updateDescDialog.html',
+        targetEvent: ev,
+        resolve: {
+          data: function() {
+            return {
+              updateDesc: targetItem.updateDesc,
+              updateId: targetItem._id
+            };
+          }
+        }
+      })
+      .then(function(result) {
+        targetItem.updateDesc = result;
+      });
+    };
+
+    RomUpdateSettingService.showDeleteUpdateDialog = function(ev, targetItem) {
+      return $mdDialog.show({
+        controller: 'RomDeleteUpdateDialogCtrl',
+        controllerAs: 'vm',
+        templateUrl: 'deleteUpdateDialog.html',
+        targetEvent: ev,
+        resolve: {
+          data: function() {
+            return {
+              targetItem: targetItem,
+              updateId: targetItem._id
+            };
+          }
+        }
+      })
+      .then(function() {
+        $state.transitionTo($state.current, $stateParams, {
+          reload: true,
+          inherit: false,
+          notify: true
+        });
+      });
+    };
+
+    RomUpdateSettingService.modifyUpdateInfo = function(versionId, updateItem) {
+      return modifyUpdateInfo(versionId, updateItem);
+    };
+
+    RomUpdateSettingService.deleteUpdateInfo = function(versionId) {
+      return $http.delete(UrlManager.getAppUpdateInfoUrl(RomUpdateSettingService.app._id) +
+        '/' + versionId + '?platform=' + RomUpdateSettingService.app.platform);
+    };
+
+    function modifyUpdateInfo(versionId, updateItem) {
+      return $http.put(UrlManager.getAppUpdateInfoUrl(RomUpdateSettingService.app._id) +
+        '/' + versionId + '?platform=' + RomUpdateSettingService.app.platform, updateItem);
+    }
+
+    function showDialog(ev, title, message) {
+      $mdDialog.show(
+        $mdDialog.alert()
+          .title(title)
+          .content(message)
+          .ariaLabel('result dialog')
+          .ok('知道了')
+          .targetEvent(ev)
+      );
+    }
 
     return RomUpdateSettingService;
   }
