f4e343e81cdd6854999d9353843fc5dfbde7d19b
rodson <rodsonluo@gmail.com>
2015-06-10 08:41:09 +0800

    add update document

--- app/dashboard/document/androidappsdk/androidappsdk.module.js
+++ app/dashboard/document/androidappsdk/androidappsdk.module.js
@@ -15,23 +15,31 @@
       })
       .state('dashboard.document.androidappsdk.getappkey', {
         url: '/getappkey',
-        templateUrl: 'app/dashboard/document/androidappsdk/integrate/getappkey.html'
+        templateUrl: 'app/dashboard/document/androidappsdk/content/getappkey.html'
       })
       .state('dashboard.document.androidappsdk.importsdk', {
         url: '/importsdk',
-        templateUrl: 'app/dashboard/document/androidappsdk/integrate/importsdk.html'
+        templateUrl: 'app/dashboard/document/androidappsdk/content/importsdk.html'
       })
       .state('dashboard.document.androidappsdk.configmanifest', {
         url: '/configmanifest',
-        templateUrl: 'app/dashboard/document/androidappsdk/integrate/configmanifest.html'
+        templateUrl: 'app/dashboard/document/androidappsdk/content/configmanifest.html'
       })
       .state('dashboard.document.androidappsdk.sessionstats', {
         url: '/sessionstats',
-        templateUrl: 'app/dashboard/document/androidappsdk/integrate/sessionstats.html'
+        templateUrl: 'app/dashboard/document/androidappsdk/content/sessionstats.html'
       })
       .state('dashboard.document.androidappsdk.customevent', {
         url: '/customevent',
-        templateUrl: 'app/dashboard/document/androidappsdk/integrate/customevent.html'
+        templateUrl: 'app/dashboard/document/androidappsdk/content/customevent.html'
+      })
+      .state('dashboard.document.androidappsdk.appupdate', {
+        url: '/appupdate',
+        templateUrl: 'app/dashboard/document/androidappsdk/content/appupdate.html'
+      })
+      .state('dashboard.document.androidappsdk.customupdate', {
+        url: '/customupdate',
+        templateUrl: 'app/dashboard/document/androidappsdk/content/customupdate.html'
       });
   }
 
